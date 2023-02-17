46a42214abe6037a70c5231aa6981124ca5315f0
SelinaHoHo <ngocthanhnbk123@gmail.com>
2023-02-17 17:38:12 +0700

    user dashboard

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Icon = (props) => {
-    const iconStyle = {
-        color: `${props.color}`,
-        fontSize: `${props.fontSize}`,
-    }
+  const iconStyle = {
+    color: `${props.color}`,
+    fontSize: `${props.fontSize}`,
+    paddingRight: `${props.paddingRight}`,
+  };
   return (
-    <div className='icon-component' style={iconStyle}>
+    <div className="icon-component" style={iconStyle}>
       <i className={`${props.content}`}></i>
     </div>
-  )
-}
+  );
+};
 
-export default Icon
+export default Icon;
