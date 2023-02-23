8a06a364b2e0a81be00c08439d71dd2f37a584a6
SelinaHoHo <ngocthanhnbk123@gmail.com>
2023-02-24 02:16:18 +0700

    category dashboard done

--- src/pages/Admin/User/CreateUser.jsx
+++ src/pages/Admin/User/CreateUser.jsx
@@ -1,95 +1,73 @@
-import React from 'react';
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
-import { Grid, GridItem } from '@chakra-ui/react';
-import Icon from '../../../components/Icon/Icon';
-import { Select } from '@chakra-ui/react';
-import ButtonBlue from '../../../components/Button/ButtonBlue';
-import { Button, ButtonGroup } from '@chakra-ui/react';
-import { history } from '../../../App';
+import React from "react";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import Icon from "../../../components/Icon/Icon";
+import { Select, Text } from "@chakra-ui/react";
+import ButtonBlue from "../../../components/Button/ButtonBlue";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { history } from "../../../App";
 const CreateUser = () => {
-   return (
-      <>
-         <center className='create-user-area'>
-            <Grid
-               h='360px'
-               w='1000px'
-               templateRows='repeat(4, 1fr)'
-               templateColumns='repeat(4, 1fr)'
-               gap={4}
+  return (
+    <>
+      <center className="create-user-area">
+        <Grid
+          h="360px"
+          w="1000px"
+          templateRows="repeat(4, 1fr)"
+          templateColumns="repeat(4, 1fr)"
+          gap={4}
+        >
+          <GridItem colStart={2} colEnd={4}>
+            {" "}
+            <Text fontSize="4xl" className="heading" colorScheme="blue">
+              Create a new user
+            </Text>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={
+                  <Icon fontSize="15px" content="fa-regular fa-envelope" />
+                }
+              />
+              <Input type="email" placeholder="Email Address" />
+            </InputGroup>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<Icon fontSize="15px" content="fa-solid fa-key" />}
+              />
+              <Input type="password" placeholder="Password" />
+            </InputGroup>
+          </GridItem>
+          <GridItem colSpan={4}>
+            <Select variant="outline" placeholder="Choose Position">
+              <option value="staff">Staff</option>
+              <option value="qaCoordinator">QA Coordinator</option>
+              <option value="qaManager">QA Manager</option>
+            </Select>
+          </GridItem>
+          <GridItem colSpan={4}>
+            <Button
+              colorScheme="red"
+              variant="ghost"
+              size="lg"
+              className="mr-3"
+              onClick={() => {
+                history.push("/user-dashboard");
+              }}
             >
-               <GridItem
-                  colStart={2}
-                  colEnd={4}
-               >
-                  {' '}
-                  <p className='title-3'>Create a new user</p>
-               </GridItem>
-               <GridItem colSpan={2}>
-                  <InputGroup>
-                     <InputLeftElement
-                        pointerEvents='none'
-                        children={
-                           <Icon
-                              fontSize='15px'
-                              content='fa-regular fa-envelope'
-                           />
-                        }
-                     />
-                     <Input
-                        type='email'
-                        placeholder='Email Address'
-                     />
-                  </InputGroup>
-               </GridItem>
-               <GridItem colSpan={2}>
-                  <InputGroup>
-                     <InputLeftElement
-                        pointerEvents='none'
-                        children={
-                           <Icon
-                              fontSize='15px'
-                              content='fa-solid fa-key'
-                           />
-                        }
-                     />
-                     <Input
-                        type='password'
-                        placeholder='Password'
-                     />
-                  </InputGroup>
-               </GridItem>
-               <GridItem colSpan={4}>
-                  <Select
-                     variant='outline'
-                     placeholder='Choose Position'
-                  >
-                     <option value='staff'>Staff</option>
-                     <option value='qaCoordinator'>QA Coordinator</option>
-                     <option value='qaManager'>QA Manager</option>
-                  </Select>
-               </GridItem>
-               <GridItem colSpan={4}>
-                  <Button
-                     colorScheme='red'
-                     variant='outline'
-                     size='md'
-                     className='mr-3'
-                     onClick={() => {
-                        history.push('/user-dashboard');
-                     }}
-                  >
-                     Back
-                  </Button>
-                  <ButtonBlue
-                     className='ml-3'
-                     padding='9px 25px'
-                     text='Create'
-                  />
-               </GridItem>
-            </Grid>
-         </center>
-      </>
-   );
+              Back
+            </Button>
+            <ButtonBlue className="ml-3" padding="9px 25px" text="Create" />
+          </GridItem>
+        </Grid>
+      </center>
+    </>
+  );
 };
 
 export default CreateUser;
--- src/pages/QaManager/Categories/CategoriesDashboard.jsx
+++ src/pages/QaManager/Categories/CategoriesDashboard.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import Icon from "../../../components/Icon/Icon";
+import { Button } from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
+import CategoryInfor from "../../../components/CategoryInfor/CategoryInfor";
 
 const CategoriesDashboard = () => {
   return (
-    <div>CategoriesDashboard</div>
-  )
-}
+    <>
+      <div className="mt-3">
+        <Grid templateColumns="repeat(4, 1fr)" gap={4}>
+          <GridItem className="ml-5 mt-4" colSpan={2} h="10" bg="#EDF2F7">
+            <InputGroup size="md" w="550">
+              <Input type="text" placeholder="Search for departments" />
+              <InputRightElement width="4.5rem">
+                <Icon fontSize="20px" content="fa-solid fa-magnifying-glass" />
+              </InputRightElement>
+            </InputGroup>
+          </GridItem>
+          <GridItem className="ml-5 mt-4 mr-5" colStart={5} colEnd={5} h="10">
+            <Button colorScheme="blue" variant="outline">
+              <Icon
+                fontSize="20px"
+                content="fa-solid fa-user-plus"
+                paddingRight="10px"
+              />
+              Create a new department
+            </Button>
+          </GridItem>
+        </Grid>
+      </div>
+      <SimpleGrid columns={2} spacingX="40px" spacingY="20px" className="m-5">
+        <CategoryInfor text="Department 1" />
+        <CategoryInfor text="Department 2" />
+        <CategoryInfor text="Department 3" />
+        <CategoryInfor text="Department 4" />
+        <CategoryInfor text="Department 5" />
+        <CategoryInfor text="Department 6" />
+        <CategoryInfor text="Department 7" />
+        <CategoryInfor text="Department 8" />
+        <CategoryInfor text="Department 9" />
+        <CategoryInfor text="Department 10" />
+        <CategoryInfor text="Department 11" />
+        <CategoryInfor text="Department 12" />
+        <CategoryInfor text="Department 13" />
+        <CategoryInfor text="Department 14" />
+      </SimpleGrid>
+    </>
+  );
+};
 
-export default CategoriesDashboard
+export default CategoriesDashboard;
