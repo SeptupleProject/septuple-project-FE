50aa9ea9d113b6aeb82aa318f3b26c3e7a836551
Ngoc Khanh Ho <99930810+unkaeciique@users.noreply.github.com>
2023-02-24 22:11:09 +0700

    Khanh (#20)
    * Idea Post completed
    
    * Like-Dislike function
    
    * Current user idea post
    
    * Current user idea post
    
    * Current user idea post

---
+++ src/components/IdeaPost/IdeaPost.jsx
@@ -0,0 +1,223 @@
+import {
+   Text,
+   Tag,
+   Button,
+   Card,
+   CardBody,
+   Divider,
+   Accordion,
+   AccordionItem,
+   AccordionPanel,
+   AccordionButton,
+   ButtonGroup,
+   HStack,
+   Input,
+   InputGroup,
+   InputRightElement,
+   IconButton
+} from '@chakra-ui/react';
+import React from 'react';
+import { useState } from 'react';
+import '../../assets/scss/main.scss';
+import gwuni from '../../assets/img/gwuni.png'
+import Icon from '../Icon/Icon';
+
+
+
+const IdeaPost = () => {
+   const [likeCount, setLikeCount] = useState(0);
+   const [dislikeCount, setDislikeCount] = useState(0);
+   const [activeBtn, setActiveBtn] = useState("none");
+
+   const handleLikeClick = () => {
+      if (activeBtn === "none") {
+        setLikeCount(likeCount + 1);
+        setActiveBtn("like");
+        return;
+      }
+   
+      if (activeBtn === 'like'){
+        setLikeCount(likeCount - 1);
+        setActiveBtn("none");
+        return;
+      }
+   
+      if (activeBtn === "dislike") {
+        setLikeCount(likeCount + 1);
+        setDislikeCount(dislikeCount - 1);
+        setActiveBtn("like");
+      }
+    };
+
+    const handleDisikeClick = () => {
+      if (activeBtn === "none") {
+        setDislikeCount(dislikeCount + 1);
+        setActiveBtn("dislike");
+        return;
+      }
+     
+      if (activeBtn === 'dislike'){
+        setDislikeCount(dislikeCount - 1);
+        setActiveBtn("none");
+        return;
+      }
+   
+      if (activeBtn === "like") {
+        setDislikeCount(dislikeCount + 1);
+        setLikeCount(likeCount - 1);
+        setActiveBtn("dislike");
+      }
+    };
+
+   return (
+      <Accordion allowToggle>
+         <Card
+            width={'92%'}
+            variant='elevated'
+         >
+
+            <CardBody className='post'>
+               {/* <div className='idea'> */}
+               <HStack spacing='13px' align='center'>
+                  <Icon
+                     content='fa-regular fa-circle-user'
+                     fontSize='62px'
+                     color='#2b6cb0'
+                     className='iconAvatar'
+                  ></Icon>
+                  <Text
+                     fontSize='4xl'
+                     className='staffName'
+                  >
+                     Segun Adebayo
+                  </Text>
+                  <Tag
+                     colorScheme='blue'
+                     size='lg'
+                     className='categoryTag'
+                  >
+                     CATEGORY 1
+                  </Tag>
+                  
+               </HStack>   
+               {/* </div> */}
+
+
+            </CardBody>
+            <AccordionItem>
+
+
+               {({ isExpanded }) => (
+                  <>
+                     <AccordionButton className=' border-0' >
+                        {isExpanded ? (
+                           <HStack>
+                              <Text
+                                 fontSize='3xl'
+                                 className='ideaTitle'
+                              >
+                                 Idea Title
+                              </Text>
+                              <Divider orientation='row' p={4} />
+                              <Button 
+                                 variant='outline'
+                                 size='lg'
+                                 colorScheme='blue'
+                              >
+                                 Collapse
+                              </Button>
+                           </HStack>
+
+                        ) : (
+                           <HStack>
+                              <Text
+                                 fontSize='3xl'
+                                 className='ideaTitle'
+                              >
+                                 Idea Title
+                              </Text>
+                              <Divider orientation='row' p={4} />
+                              <Button variant='solid'
+                                 size='lg'
+                                 colorScheme='blue'
+                              >
+                                 Reveal
+                              </Button>
+                           </HStack>
+
+                        )}
+                     </AccordionButton>
+
+                     <AccordionPanel className='hiddenPanel'>
+                        <div className='ideaPara'>
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique sollicitudin nibh. Pharetra pharetra massa massa ultricies mi. Cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Libero justo laoreet sit amet cursus sit. In ornare quam viverra orci sagittis. Ac felis donec et odio pellentesque diam volutpat commodo sed. Gravida neque convallis a cras semper. Massa sapien faucibus et molestie ac feugiat. Duis at tellus at urna. Neque viverra justo nec ultrices dui sapien eget mi proin. Cras fermentum odio eu feugiat pretium nibh. Et odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Habitant morbi tristique senectus et netus et malesuada fames ac. In fermentum posuere urna nec tincidunt. Adipiscing at in tellus integer feugiat scelerisque. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Diam quis enim lobortis scelerisque fermentum. Ac tortor vitae purus faucibus ornare suspendisse sed. Arcu non odio euismod lacinia at quis risus sed.
+                        </div>
+
+                        <img
+                           src={gwuni}
+                           alt='University of Greenwich'
+                           className='image'
+                        />
+
+                        <ButtonGroup variant='ghost' size='lg'>
+                           <Button 
+                              colorScheme='blue'
+                              leftIcon={<Icon content='fa-regular fa-thumbs-up' />} 
+                              onClick={handleLikeClick}
+                              className={`${activeBtn === "like" ? "like-active" : ""}`}
+                           >
+                              {likeCount}
+                           </Button>
+                           <Button 
+                              colorScheme='red'
+                              leftIcon={<Icon content='fa-regular fa-thumbs-down' />}
+                              onClick={handleDisikeClick}
+                              className={`${activeBtn === "dislike" ? "dislike-active" : ""}`}
+                           >
+                              {dislikeCount}
+                           </Button>
+                           <Button 
+                              isDisabled
+                              leftIcon={<Icon content='fa-regular fa-comment-dots' />}
+                              colorScheme='black'
+                           >  
+                              0
+                           </Button>
+                        </ButtonGroup>
+                        <Divider />
+                        <HStack>
+                           <Icon
+                              content='fa-regular fa-circle-user'
+                              fontSize='34px'
+                              color='#2b6cb0'
+                           ></Icon>
+                           <InputGroup>
+                              <Input 
+                                 placeholder='What do you think?' 
+                                 variant='outline'
+                                 borderRadius={'20px'} />
+                              <InputRightElement>
+                                 <IconButton 
+                                    icon={<Icon 
+                                             content='fa-regular fa-paper-plane' 
+                                             fontSize='15px' />}
+                                    variant='ghost'
+                                    colorScheme='blue'
+                                    borderRadius={'20px'} />
+                              </InputRightElement>
+                           </InputGroup>
+   
+                        </HStack>
+                        
+
+                     </AccordionPanel>
+                  </>
+               )}
+            </AccordionItem>
+         </Card>
+      </Accordion>
+
+   );
+};
+
+export default IdeaPost;
---
+++ src/components/IdeaPost/YourIdeaPost.jsx
@@ -0,0 +1,315 @@
+import {
+  Text,
+  Tag,
+  Button,
+  Card,
+  CardBody,
+  Divider,
+  ButtonGroup,
+  HStack,
+  Input,
+  InputGroup,
+  InputRightElement,
+  IconButton,
+  useDisclosure,
+  SlideFade,
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  Editable,
+  EditablePreview,
+  EditableTextarea,
+  EditableInput,
+  Alert,
+  Switch,
+  VStack,
+} from "@chakra-ui/react";
+import React from "react";
+import { useState } from "react";
+import "../../assets/scss/main.scss";
+import gwuni from "../../assets/img/gwuni.png";
+import Icon from "../Icon/Icon";
+
+const YourIdeaPost = () => {
+  const [likeCount, setLikeCount] = useState(0);
+  const [dislikeCount, setDislikeCount] = useState(0);
+  const [activeBtn, setActiveBtn] = useState("none");
+
+  const [lock, setLock] = useState(false);
+
+  const { isOpen, onToggle } = useDisclosure();
+
+  const [uploadImg, setUploadImg] = useState(gwuni);
+
+  const handleOnChange = () => {
+    setLock(!lock);
+  };
+
+  const handleUploadImage = (e) => {
+    let file = e.target.files[0];
+    if (
+      file.type === "image/jpeg" ||
+      file.type === "image/jpg" ||
+      file.type === "image/png"
+    ) {
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (event) => setUploadImg(event.target.result);
+    }
+  };
+
+  const handleLikeClick = () => {
+    if (activeBtn === "none") {
+      setLikeCount(likeCount + 1);
+      setActiveBtn("like");
+      return;
+    }
+
+    if (activeBtn === "like") {
+      setLikeCount(likeCount - 1);
+      setActiveBtn("none");
+      return;
+    }
+
+    if (activeBtn === "dislike") {
+      setLikeCount(likeCount + 1);
+      setDislikeCount(dislikeCount - 1);
+      setActiveBtn("like");
+    }
+  };
+
+  const handleDisikeClick = () => {
+    if (activeBtn === "none") {
+      setDislikeCount(dislikeCount + 1);
+      setActiveBtn("dislike");
+      return;
+    }
+
+    if (activeBtn === "dislike") {
+      setDislikeCount(dislikeCount - 1);
+      setActiveBtn("none");
+      return;
+    }
+
+    if (activeBtn === "like") {
+      setDislikeCount(dislikeCount + 1);
+      setLikeCount(likeCount - 1);
+      setActiveBtn("dislike");
+    }
+  };
+
+  return (
+    <Accordion allowToggle>
+      <Card width={"92%"} variant="elevated">
+        <CardBody className="post">
+          <HStack spacing="13px" align="center" paddingBottom={"0"}>
+            <Icon
+              content="fa-regular fa-circle-user"
+              fontSize="62px"
+              color="#2b6cb0"
+              className="iconAvatar"
+            ></Icon>
+            <Text fontSize="4xl" className="staffName">
+              You
+            </Text>
+            <Tag colorScheme="blue" size="lg" className="categoryTag">
+              CATEGORY 1
+            </Tag>
+          </HStack>
+        </CardBody>
+
+        <AccordionItem className="border-0">
+          {({ isExpanded }) => (
+            <>
+              <HStack>
+                <Editable
+                  fontSize="3xl"
+                  className="ideaTitle"
+                  placeholder="Idea Title"
+                  marginLeft={"1%"}
+                >
+                  <EditablePreview />
+                  <EditableInput />
+                </Editable>
+
+                {isExpanded ? (
+                  <>
+                    <AccordionButton width={"10%"} _hover={{ bgColor: "none" }}>
+                      <HStack>
+                        <Button
+                          variant="solid"
+                          size="lg"
+                          colorScheme="linkedin"
+                        >
+                          Collapse
+                        </Button>
+                      </HStack>
+                    </AccordionButton>
+                    <Button
+                      variant="outline"
+                      size="md"
+                      colorScheme="red"
+                      onClick={onToggle}
+                    >
+                      Delete Post
+                    </Button>
+
+                    <SlideFade in={isOpen} marginLeft={"3%"}>
+                      <HStack>
+                        <Text size="md" className="deleteBtn">
+                          Are you sure?
+                        </Text>
+                        <ButtonGroup variant="ghost" size="sm">
+                          <Button colorScheme="red">Yes</Button>
+                          <Button colorScheme="twitter">No</Button>
+                        </ButtonGroup>
+                      </HStack>
+                    </SlideFade>
+                  </>
+                ) : (
+                  <AccordionButton width={"10%"} _hover={{ bgColor: "none" }}>
+                    <HStack>
+                      <Button variant="solid" size="lg" colorScheme="blue">
+                        Reveal
+                      </Button>
+                    </HStack>
+                  </AccordionButton>
+                )}
+              </HStack>
+
+              <AccordionPanel className="hiddenPanel">
+                <Editable
+                  className="editablePara"
+                  placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Potenti nullam ac tortor vitae purus faucibus. Sagittis id consectetur purus ut faucibus. Dolor purus non enim praesent. Amet nisl suscipit adipiscing bibendum est ultricies integer. Cras tincidunt lobortis feugiat vivamus at augue eget. Praesent semper feugiat nibh sed pulvinar proin gravida. Tincidunt ornare massa eget egestas. Tellus at urna condimentum mattis. Condimentum vitae sapien pellentesque habitant morbi. Arcu dictum varius duis at consectetur lorem donec massa. Ante metus dictum at tempor commodo ullamcorper a lacus vestibulum. Porta lorem mollis aliquam ut. Nisl vel pretium lectus quam."
+                >
+                  <EditablePreview />
+                  <EditableTextarea maxHeight={"max-content"} />
+                </Editable>
+
+                <HStack>
+                  <img
+                    src={uploadImg}
+                    alt="University of Greenwich"
+                    className="img-fluid image"
+                  />
+
+                  <div className="col-2 px-0 mx-0 text-center">
+                    <div className="container w-75 ">
+                      <label
+                        htmlFor="uploadImageToEdid"
+                        className="d-flex uploadBtn"
+                      >
+                        <HStack className="uploadStack">
+                          <Icon content="fa-regular fa-image" color="#3182ce" />
+                          <p className="ml-1 uploadBtnText">Change photo</p>
+                        </HStack>
+                      </label>
+                      <input
+                        onChange={handleUploadImage}
+                        accept="image/png,image/jpg,image/jpeg"
+                        className="disapear"
+                        id="uploadImageToEdid"
+                        type="file"
+                      />
+                    </div>
+                    <div className="my-3">
+                      <b>Post Anonymously ?</b>
+                    </div>
+                    <div className="d-flex justify-content-center align-middle">
+                      <Switch
+                        size="sm"
+                        className="p-0 mt-1 mr-3"
+                        onChange={handleOnChange}
+                      />
+
+                      <Icon
+                        content={
+                          lock ? "fa-solid fa-lock" : "fa-solid fa-lock-open"
+                        }
+                        fontSize="15px"
+                      />
+                    </div>
+                    <VStack>
+                      <Button
+                        variant="outline"
+                        size="sm"
+                        colorScheme="green"
+                        onClick={() => {
+                          setUploadImg();
+                        }}
+                      >
+                        Accept changes
+                      </Button>
+                      <Button colorScheme="red" variant="outline" size="sm">
+                        Cancel changes
+                      </Button>
+                    </VStack>
+                  </div>
+                </HStack>
+
+                <ButtonGroup variant="ghost" size="lg">
+                  <Button
+                    colorScheme="blue"
+                    leftIcon={<Icon content="fa-regular fa-thumbs-up" />}
+                    onClick={handleLikeClick}
+                    className={`${activeBtn === "like" ? "like-active" : ""}`}
+                  >
+                    {likeCount}
+                  </Button>
+                  <Button
+                    colorScheme="red"
+                    leftIcon={<Icon content="fa-regular fa-thumbs-down" />}
+                    onClick={handleDisikeClick}
+                    className={`${
+                      activeBtn === "dislike" ? "dislike-active" : ""
+                    }`}
+                  >
+                    {dislikeCount}
+                  </Button>
+                  <Button
+                    isDisabled
+                    leftIcon={<Icon content="fa-regular fa-comment-dots" />}
+                    colorScheme="black"
+                  >
+                    0
+                  </Button>
+                </ButtonGroup>
+                <Divider />
+                <HStack>
+                  <Icon
+                    content="fa-regular fa-circle-user"
+                    fontSize="34px"
+                    color="#2b6cb0"
+                  ></Icon>
+                  <InputGroup>
+                    <Input
+                      placeholder="What do you think?"
+                      variant="outline"
+                      borderRadius={"20px"}
+                    />
+                    <InputRightElement>
+                      <IconButton
+                        icon={
+                          <Icon
+                            content="fa-regular fa-paper-plane"
+                            fontSize="15px"
+                          />
+                        }
+                        variant="ghost"
+                        colorScheme="blue"
+                        borderRadius={"20px"}
+                      />
+                    </InputRightElement>
+                  </InputGroup>
+                </HStack>
+              </AccordionPanel>
+            </>
+          )}
+        </AccordionItem>
+      </Card>
+    </Accordion>
+  );
+};
+
+export default YourIdeaPost;
