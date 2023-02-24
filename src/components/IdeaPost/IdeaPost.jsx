53645fc3b239f1ef70b5336979361981be8981d9
KhaDoan209 <doanvinhkha@gmail.com>
2023-02-25 00:14:46 +0700

    hot fix post

--- src/components/IdeaPost/IdeaPost.jsx
+++ src/components/IdeaPost/IdeaPost.jsx
@@ -14,143 +14,177 @@ import {
    Input,
    InputGroup,
    InputRightElement,
-   IconButton
+   IconButton,
 } from '@chakra-ui/react';
 import React from 'react';
 import { useState } from 'react';
 import '../../assets/scss/main.scss';
-import gwuni from '../../assets/img/gwuni.png'
+import gwuni from '../../assets/img/gwuni.png';
 import Icon from '../Icon/Icon';
-
-
 
 const IdeaPost = () => {
    const [likeCount, setLikeCount] = useState(0);
    const [dislikeCount, setDislikeCount] = useState(0);
-   const [activeBtn, setActiveBtn] = useState("none");
+   const [activeBtn, setActiveBtn] = useState('none');
 
    const handleLikeClick = () => {
-      if (activeBtn === "none") {
-        setLikeCount(likeCount + 1);
-        setActiveBtn("like");
-        return;
+      if (activeBtn === 'none') {
+         setLikeCount(likeCount + 1);
+         setActiveBtn('like');
+         return;
       }
-   
-      if (activeBtn === 'like'){
-        setLikeCount(likeCount - 1);
-        setActiveBtn("none");
-        return;
-      }
-   
-      if (activeBtn === "dislike") {
-        setLikeCount(likeCount + 1);
-        setDislikeCount(dislikeCount - 1);
-        setActiveBtn("like");
-      }
-    };
 
-    const handleDisikeClick = () => {
-      if (activeBtn === "none") {
-        setDislikeCount(dislikeCount + 1);
-        setActiveBtn("dislike");
-        return;
+      if (activeBtn === 'like') {
+         setLikeCount(likeCount - 1);
+         setActiveBtn('none');
+         return;
       }
-     
-      if (activeBtn === 'dislike'){
-        setDislikeCount(dislikeCount - 1);
-        setActiveBtn("none");
-        return;
+
+      if (activeBtn === 'dislike') {
+         setLikeCount(likeCount + 1);
+         setDislikeCount(dislikeCount - 1);
+         setActiveBtn('like');
       }
-   
-      if (activeBtn === "like") {
-        setDislikeCount(dislikeCount + 1);
-        setLikeCount(likeCount - 1);
-        setActiveBtn("dislike");
+   };
+
+   const handleDisikeClick = () => {
+      if (activeBtn === 'none') {
+         setDislikeCount(dislikeCount + 1);
+         setActiveBtn('dislike');
+         return;
       }
-    };
+
+      if (activeBtn === 'dislike') {
+         setDislikeCount(dislikeCount - 1);
+         setActiveBtn('none');
+         return;
+      }
+
+      if (activeBtn === 'like') {
+         setDislikeCount(dislikeCount + 1);
+         setLikeCount(likeCount - 1);
+         setActiveBtn('dislike');
+      }
+   };
 
    return (
       <Accordion allowToggle>
-         <Card
-            width={'92%'}
-            variant='elevated'
-         >
-
+         <Card variant='elevated'>
             <CardBody className='post'>
-               {/* <div className='idea'> */}
-               <HStack spacing='13px' align='center'>
+               <HStack
+                  spacing='13px'
+                  align='center'
+               >
                   <Icon
                      content='fa-regular fa-circle-user'
-                     fontSize='62px'
+                     fontSize='45px'
                      color='#2b6cb0'
                      className='iconAvatar'
                   ></Icon>
                   <Text
-                     fontSize='4xl'
+                     fontSize='3xl'
                      className='staffName'
                   >
                      Segun Adebayo
                   </Text>
                   <Tag
                      colorScheme='blue'
-                     size='lg'
+                     size='md'
                      className='categoryTag'
                   >
                      CATEGORY 1
                   </Tag>
-                  
-               </HStack>   
-               {/* </div> */}
-
-
+               </HStack>
             </CardBody>
-            <AccordionItem>
 
-
+            <AccordionItem
+               style={{
+                  borderTop: '2px solid rgba(43, 107, 177,0.6)',
+                  borderTopRightRadius: '8px',
+                  borderTopLeftRadius: '8px',
+                  borderBottom: 'none',
+               }}
+            >
                {({ isExpanded }) => (
                   <>
-                     <AccordionButton className=' border-0' >
+                     <AccordionButton
+                        style={{ width: 'fit-content' }}
+                        className='m-2'
+                     >
                         {isExpanded ? (
                            <HStack>
                               <Text
-                                 fontSize='3xl'
+                                 fontSize='2xl'
                                  className='ideaTitle'
                               >
                                  Idea Title
                               </Text>
-                              <Divider orientation='row' p={4} />
-                              <Button 
-                                 variant='outline'
-                                 size='lg'
-                                 colorScheme='blue'
-                              >
-                                 Collapse
-                              </Button>
-                           </HStack>
+                              <Divider
+                                 orientation='row'
+                                 p={4}
+                              />
 
+                              <div className=''>
+                                 <IconButton
+                                    className='m-0 p-0'
+                                    colorScheme='blue'
+                                    aria-label='Search database'
+                                    variant='outline'
+                                    icon={
+                                       <Icon content='fa-solid fa-eye-slash' />
+                                    }
+                                 />
+                              </div>
+                           </HStack>
                         ) : (
                            <HStack>
                               <Text
-                                 fontSize='3xl'
+                                 fontSize='2xl'
                                  className='ideaTitle'
                               >
                                  Idea Title
                               </Text>
-                              <Divider orientation='row' p={4} />
-                              <Button variant='solid'
-                                 size='lg'
-                                 colorScheme='blue'
-                              >
-                                 Reveal
-                              </Button>
+                              <Divider
+                                 orientation='row'
+                                 p={4}
+                              />
+                              <div>
+                                 <IconButton
+                                    className='m-0 p-0'
+                                    colorScheme='blue'
+                                    aria-label='Search database'
+                                    variant='outline'
+                                    icon={<Icon content='fa-solid fa-eye' />}
+                                 />
+                              </div>
                            </HStack>
-
                         )}
                      </AccordionButton>
 
                      <AccordionPanel className='hiddenPanel'>
                         <div className='ideaPara'>
-                           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur adipiscing elit duis tristique sollicitudin nibh. Pharetra pharetra massa massa ultricies mi. Cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Libero justo laoreet sit amet cursus sit. In ornare quam viverra orci sagittis. Ac felis donec et odio pellentesque diam volutpat commodo sed. Gravida neque convallis a cras semper. Massa sapien faucibus et molestie ac feugiat. Duis at tellus at urna. Neque viverra justo nec ultrices dui sapien eget mi proin. Cras fermentum odio eu feugiat pretium nibh. Et odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Habitant morbi tristique senectus et netus et malesuada fames ac. In fermentum posuere urna nec tincidunt. Adipiscing at in tellus integer feugiat scelerisque. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Diam quis enim lobortis scelerisque fermentum. Ac tortor vitae purus faucibus ornare suspendisse sed. Arcu non odio euismod lacinia at quis risus sed.
+                           Lorem ipsum dolor sit amet, consectetur adipiscing
+                           elit, sed do eiusmod tempor incididunt ut labore et
+                           dolore magna aliqua. Consectetur adipiscing elit duis
+                           tristique sollicitudin nibh. Pharetra pharetra massa
+                           massa ultricies mi. Cras fermentum odio eu feugiat
+                           pretium nibh ipsum consequat nisl. Libero justo
+                           laoreet sit amet cursus sit. In ornare quam viverra
+                           orci sagittis. Ac felis donec et odio pellentesque
+                           diam volutpat commodo sed. Gravida neque convallis a
+                           cras semper. Massa sapien faucibus et molestie ac
+                           feugiat. Duis at tellus at urna. Neque viverra justo
+                           nec ultrices dui sapien eget mi proin. Cras fermentum
+                           odio eu feugiat pretium nibh. Et odio pellentesque
+                           diam volutpat commodo sed egestas egestas fringilla.
+                           Habitant morbi tristique senectus et netus et
+                           malesuada fames ac. In fermentum posuere urna nec
+                           tincidunt. Adipiscing at in tellus integer feugiat
+                           scelerisque. Platea dictumst vestibulum rhoncus est
+                           pellentesque elit ullamcorper dignissim. Diam quis
+                           enim lobortis scelerisque fermentum. Ac tortor vitae
+                           purus faucibus ornare suspendisse sed. Arcu non odio
+                           euismod lacinia at quis risus sed.
                         </div>
 
                         <img
@@ -159,28 +193,41 @@ const IdeaPost = () => {
                            className='image'
                         />
 
-                        <ButtonGroup variant='ghost' size='lg'>
-                           <Button 
+                        <ButtonGroup
+                           variant='ghost'
+                           size='lg'
+                        >
+                           <Button
                               colorScheme='blue'
-                              leftIcon={<Icon content='fa-regular fa-thumbs-up' />} 
+                              leftIcon={
+                                 <Icon content='fa-regular fa-thumbs-up' />
+                              }
                               onClick={handleLikeClick}
-                              className={`${activeBtn === "like" ? "like-active" : ""}`}
+                              className={`${
+                                 activeBtn === 'like' ? 'like-active' : ''
+                              }`}
                            >
                               {likeCount}
                            </Button>
-                           <Button 
+                           <Button
                               colorScheme='red'
-                              leftIcon={<Icon content='fa-regular fa-thumbs-down' />}
+                              leftIcon={
+                                 <Icon content='fa-regular fa-thumbs-down' />
+                              }
                               onClick={handleDisikeClick}
-                              className={`${activeBtn === "dislike" ? "dislike-active" : ""}`}
+                              className={`${
+                                 activeBtn === 'dislike' ? 'dislike-active' : ''
+                              }`}
                            >
                               {dislikeCount}
                            </Button>
-                           <Button 
+                           <Button
                               isDisabled
-                              leftIcon={<Icon content='fa-regular fa-comment-dots' />}
+                              leftIcon={
+                                 <Icon content='fa-regular fa-comment-dots' />
+                              }
                               colorScheme='black'
-                           >  
+                           >
                               0
                            </Button>
                         </ButtonGroup>
@@ -192,31 +239,32 @@ const IdeaPost = () => {
                               color='#2b6cb0'
                            ></Icon>
                            <InputGroup>
-                              <Input 
-                                 placeholder='What do you think?' 
+                              <Input
+                                 placeholder='What do you think?'
                                  variant='outline'
-                                 borderRadius={'20px'} />
+                                 borderRadius={'20px'}
+                              />
                               <InputRightElement>
-                                 <IconButton 
-                                    icon={<Icon 
-                                             content='fa-regular fa-paper-plane' 
-                                             fontSize='15px' />}
+                                 <IconButton
+                                    icon={
+                                       <Icon
+                                          content='fa-regular fa-paper-plane'
+                                          fontSize='15px'
+                                       />
+                                    }
                                     variant='ghost'
                                     colorScheme='blue'
-                                    borderRadius={'20px'} />
+                                    borderRadius={'20px'}
+                                 />
                               </InputRightElement>
                            </InputGroup>
-   
                         </HStack>
-                        
-
                      </AccordionPanel>
                   </>
                )}
             </AccordionItem>
          </Card>
       </Accordion>
-
    );
 };
 
